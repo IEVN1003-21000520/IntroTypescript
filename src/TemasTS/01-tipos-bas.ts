66526683d27c33ed113efd8b2ad78a174b7af2eb
Rocio Luna <rosilunna19@gmail.com>
2026-09-05 07:20:29 -0600

    Tipos de datos 04-09-2026

---
+++ src/TemasTS/01-tipos-bas.ts
@@ -0,0 +1,19 @@
+console.log("Hola mundo!!");
+
+var num1=23;
+let num2=65;
+const num3=22;
+
+Let nombre:string;
+Let num:number
+Let activo:boolean
+Let sum:number=num1+num2+num3
+console.log("La suma es : "+sum)
+
+nombre="Pedro";
+console.log(nombre);
+
+
+console.log(nombre);
+console.log("Prueba")
+
